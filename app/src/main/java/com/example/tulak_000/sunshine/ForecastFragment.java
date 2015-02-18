9e3c92729aa54b0499c84ce16146dc3048917604
Tulakan Ruangrong <tulakan.r@gmail.com>
2015-02-18 14:05:02 +0700

    add toast with mock params

--- app/src/main/java/com/example/tulak_000/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/tulak_000/sunshine/ForecastFragment.java
@@ -16,8 +16,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,7 +88,7 @@ public class ForecastFragment extends Fragment {
 
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
-       forecastAdapter =
+        forecastAdapter =
                 new ArrayAdapter<String>(
                         getActivity(),
                         R.layout.list_item_forecast,
@@ -94,8 +96,18 @@ public class ForecastFragment extends Fragment {
                         weekForecast
                 );
 
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        
+        // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
+                String forecast = forecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
